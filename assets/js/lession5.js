1ecefc2e89f3c7a808edc395263cfc7015377d50
buimtuan <tuanzj@yahoo.com>
2020-11-01 20:06:52 -0600

    fix js

--- assets/js/lession5.js
+++ assets/js/lession5.js
@@ -1,77 +1,39 @@
-/*
-	For each of the following, write at least one test case for each function.  They should all be nicely formatted. Do your development in Intellij and also see the further instructions below.    If you follow those instructions you can create an html page with your JavaScript in that page and then post it to your site.
-    Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
-    Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
-    Write a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
-    Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
-    Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
-    Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
-    Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
-
-    Modify the jsfiddle on the map/filter/reduce slide as follows:
-    a) multiply each element by 10;
-    b) return array with all elements equal to 3;
-    c) return the product of all elements;
-    ​After you finish, make sure all your functions are in ONE JS file .
-Additional points
-    Your assignment report should include a comment on how long it took, and describe something that you learned from the exercise or some problem you encountered, etc.
-    For each function you write you should show some test output.  E.g., you should have a series of console.log statements such as the following:
-        console.log("Expected output of max(20,10) is 10 and  " +
-           myFunctionTest(10, function(){return max( 20, 10); );
-        the first argument to myFunctionTest should be the expected value for the test
-        the second argument to myFunctionTest should be an anonymous function expression containing a single return statement, which returns the result of the function call with the test arguments
-        myFunctionTest should return "TEST SUCCEEDED" or "TEST FAILED"
-        in addition to myFunctionTest you should use console.assert for the same inputs and expected values for each myFunctionTest
-            This is somewhat repetitive to myFunctionTest.  It is another means of testing and validating your code.
-    see the sakai Resources folder "lab helpers"/JSintroLab.html for an example of using the myFunctionTest
-
-For example, your output might look like the following:
-*/
 
 function max(a, b) {
 	return a > b ? a : b;
 }
+
 function maxOfThree(a, b, c) {
-	let tmp = a > b ? a : b;
-	return tmp > c ? tmp : c;
+	return max(max(a, b), c);
 }
+
 function isVowel(c) {
-	let a = c.charCodeAt(0);
-	return (a > 96 && a < 123) ? true : false;
+	const vowels = ['a', 'e', 'i','o','u'];
+	return vowels.includes(c);
 }
+
 function sum(arr) {
-	let rs = 0;
-	for (let i = 0; i < arr.length; ++i) {
-		rs += arr[i];
-	}
-	return rs;
+	return arr.reduce((accum, elem) => accum + elem);
 }
+
 function multiply(arr) {
-	let rs = 0;
-	for (let i = 0; i < arr.length; ++i) {
-		rs *= arr[i];
-	}
-	return rs;
+	return arr.reduce((accum, elem) => accum * elem);
 }
+
 function reverse(str) {
-	let splitString = str.split('');
-	let reverseArray = splitString.reverse();
-    let joinArray = reverseArray.join("");
-	return joinArray;
+	return str.split('').reverse().join();
 }
 
 function findLongestWord(str) {
-	let splitString = str.split(' ');
-	let longestWord = splitString.reduce(
+	let longestWord = str.split(' ').reduce(
 		(longest, currentWord) => currentWord.length > longest.length ? currentWord : longest,
 		""
 	);
 	return longestWord.length;
 }
+
 function filterLongWords(str, size) {
-	let splitString = str.split(' ');
-	const rs = splitString.filter(word => word.length > size)
-	return rs;
+	return str.split(' ').filter(word => word.length > size);
 }
 
 function jsfiddle(arr) {
